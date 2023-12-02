4b441593592a1530ce32e7b254563a772e30572a
Sagarjainjain <sagarjain.jain10@gmail.com>
2023-12-02 12:03:23 +0530

    sixth_commit

--- app/profile/tray/page.jsx
+++ app/profile/tray/page.jsx
@@ -1,7 +1,26 @@
+"use client";
 import itemintray from "../../../assets/api/demotray.json";
+import { IoIosArrowDown } from "react-icons/io";
 import "./tray.css";
+import { useState } from "react";
 
 const Tray = () => {
+  const [collapsible, setcollapsible] = useState(false);
+  let gst = 3;
+  let NetAmount = 0;
+  if (itemintray.length > 0) {
+    const total = itemintray.reduce((accumulator, item) => {
+      const itemPrice = item.quanity * item.product.price;
+      NetAmount += itemPrice;
+      return accumulator + itemPrice;
+    }, 0);
+  } else {
+    console.log("itemintray is empty");
+  }
+
+  let taxcalculate = (NetAmount * gst) / 100;
+  let Subtotal = NetAmount + taxcalculate + taxcalculate;
+
   return (
     <div className="tray_container flex__center">
       <div className="tray_title">
@@ -31,7 +50,29 @@ const Tray = () => {
         </div>
         <div className="tray_subtotal_container">
           <div className="tray_subtotal-card">
-            <h5>subtotal</h5>
+            <div
+              className="tray_subtotal-card_total"
+              onClick={() => setcollapsible((prev) => !prev)}
+            >
+              {collapsible ? (
+                <h5>Net Amount: ₹{NetAmount}/-</h5>
+              ) : (
+                <h5>Subtotal: ₹{Math.ceil(Subtotal)}/-</h5>
+              )}
+              <IoIosArrowDown size={20} />
+            </div>
+            {collapsible ? (
+              <div className="tray_subtotal-card_taxes">
+                <p>CGST: 3%</p>
+                <p>SGST: 3%</p>
+                <h4>Subtotal: ₹{Math.ceil(Subtotal)}/-</h4>
+              </div>
+            ) : (
+              <div />
+            )}
+            <div className="tray_subtotal-footer">
+              <button>Proceed to Checkout</button>
+            </div>
           </div>
         </div>
       </div>
